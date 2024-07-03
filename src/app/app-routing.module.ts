a95a82956f13aaa9616e9e584341e5b344dbf2b3
sthembizo.molefi <Sthembizo@psybergate.co.za>
2024-07-03 23:43:08 +0200

    added routing to note detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes,provideRouter} from '@angular/router';
 import { NotesListComponent } from './features/notes/components/ui/notes-list/notes-list.component';
 import { AddNoteComponent } from './features/notes/components/smart/add-note/add-note.component';
+import { NoteDetailComponent } from './features/notes/components/ui/note-detail/note-detail.component';
 
 const routes: Routes = [
   { path: '', component: NotesListComponent },
   { path: 'new', component: AddNoteComponent },
+  { path: 'note', component: NoteDetailComponent },
 ];
 
 

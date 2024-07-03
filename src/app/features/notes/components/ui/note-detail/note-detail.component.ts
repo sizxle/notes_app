a25b2bfa1a0524c4f89dd7a36ef37674951608c5
sthembizo.molefi <Sthembizo@psybergate.co.za>
2024-07-04 00:23:44 +0200

    retrieving the state from the front

--- src/app/features/notes/components/ui/note-detail/note-detail.component.ts
+++ src/app/features/notes/components/ui/note-detail/note-detail.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { Note } from '../../../models/notes';
 @Component({
   selector: 'app-note-detail',
   templateUrl: './note-detail.component.html',
   styleUrl: './note-detail.component.scss'
 })
 export class NoteDetailComponent {
+
+  note!:Note;
+
+  constructor(private router: Router) {
+    const navigation= this.router.getCurrentNavigation();
+    if (navigation?.extras.state) {
+      this.note = navigation.extras.state['note'];
+    }
+   }
+
 
 }

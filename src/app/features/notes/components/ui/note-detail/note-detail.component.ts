706852bfccc8d0704e585e039890e4aa87c5b949
sthembizo.molefi <Sthembizo@psybergate.co.za>
2024-07-04 01:52:26 +0200

    changing the handling of routes

--- src/app/features/notes/components/ui/note-detail/note-detail.component.ts
+++ src/app/features/notes/components/ui/note-detail/note-detail.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from "@angular/router";
-import { Note } from '../../../models/notes';
+import { Router, ActivatedRoute } from "@angular/router";
+import { Note, NOTES } from '../../../models/notes';
 @Component({
   selector: 'app-note-detail',
   templateUrl: './note-detail.component.html',
   styleUrl: './note-detail.component.scss'
 })
-export class NoteDetailComponent {
+export class NoteDetailComponent implements OnInit{
 
-  note!:Note;
+  note?:Note;
 
-  constructor(private router: Router) {
-    const navigation= this.router.getCurrentNavigation();
-    if (navigation?.extras.state) {
-      this.note = navigation.extras.state['note'];
-    }
-   }
+  constructor(private router: Router,private activeRoute: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    let id =Number(this.activeRoute.snapshot.paramMap.get('id'));
+    this.note= NOTES.find(note => note.id === id);
+  }
 
 }
--- src/app/features/notes/components/ui/note-detail/note-detail.module.ts
+++ src/app/features/notes/components/ui/note-detail/note-detail.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NoteDetailComponent } from './add-note.component';
+import { NoteDetailComponent } from './note-detail.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ NoteDetailComponent ],
-  imports: [CommonModule,],
+  imports: [CommonModule,
+    RouterModule,
+  ],
   exports: [ NoteDetailComponent ]
 })
 export class NoteDetailModule {}
